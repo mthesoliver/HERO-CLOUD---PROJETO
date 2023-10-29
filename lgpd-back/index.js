b11f7d83a0cef67b85bf7d3ab0005e4d3359c846
mthesoliver <m.limaoliveira96@gmail.com>
2023-10-29 17:24:31 -0300

    Add integração front com back na nova branch backend

---
+++ lgpd-back/index.js
@@ -0,0 +1,30 @@
+import { express } from "express";
+import { pkg } from "body-parser";
+import { router } from "./routes/router.js";
+
+import sequelize from "./lgpd-back/utils/database.js";
+import association from "./lgpd-back/models/Associations.js"
+
+import cors from "cors"
+
+const app = express();
+const { json, urlencoded } = pkg;
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use("/" , router);
+app.use(cors());
+
+(async () => {
+    try {
+        association.associations();
+        await sequelize.sync();
+        app.listen(3000, function () {
+            console.log("listening from 3000");
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+app.use("/", router);
