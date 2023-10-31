bbb7e1f2311a43a026e5b4f271bfc2a980dd9b3d
mthesoliver <m.limaoliveira96@gmail.com>
2023-10-30 21:37:33 -0300

    add recurso de envio de arquivo

--- lgpd-back/routes/UserController.js
+++ lgpd-back/routes/UserController.js
@@ -1,41 +1,56 @@
-import  express  from "express";
+import express from "express";
 let router = express.Router();
 
 import userService from "../services/UserService.js";
+import multer from "multer";
+import process from "process";
 
-router.post("/addUser" , async function(req, res) {
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './images');
+    },
+    filename: function (req, file, callback) {
+        callback(null, req.body.first_name + "_" + req.body.last_name + "_" + Date.now() + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage }).single('file');
+
+router.post("/addUser", async function (req, res) {
     const userModel = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email,
-        gender: req.body.gender
+        gender: req.body.gender,
+        profile_picture: req.file.path
     }
 
     const user = await userService.saveUser(userModel);
     return res.status(200).json(user);
 });
 
-router.get("/getAllUsers", async function (req, res){
+router.get("/getAllUsers", async function (req, res) {
     const allUsers = await userService.getAllUsers();
     return res.status(200).json(allUsers);
 });
 
-router.get("/user/:id", async function (req, res){
+router.get("/user/:id", async function (req, res) {
     const user = await userService.getUserById(req.params.id);
     return res.status(200).json(user);
 });
 
-router.delete("/delete/:id", async function (req, res){
+router.delete("/delete/:id", async function (req, res) {
     const user = await userService.deleteUserById(req.params.id);
     return res.status(200).json(user);
 });
 
-router.put("/updateUser/:id" , async function(req, res) {
+router.put("/updateUser/:id", async function (req, res) {
     const userModel = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email,
-        gender: req.body.gender
+        gender: req.body.gender,
+        profile_picture: req.file.path
     }
 
     const user = await userService.updateUserById(req.params.id, userModel);
